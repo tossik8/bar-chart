73a36bf6ebc19ef7d89764f88841ccc83314b6f3
user <toropov.nikita2004@gmail.com>
2023-01-29 11:22:16 +0100

    Change the order of execution

--- bar-chart.js
+++ bar-chart.js
@@ -4,49 +4,17 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     .then(response => response.json())
     .then(d => createGraph(d));
 
-
-window.onload = () => {
-    const rects = document.getElementsByTagName("rect");
-    document.getElementById("tooltip")
-    for(let rect of rects){
-        rect.onmouseover = () => {
-            document.getElementById("tooltip").setAttribute("data-date", rect.attributes.getNamedItem("data-date").value);
-            document.getElementById("info").innerText = rect.attributes.getNamedItem("data-date").value + "\n$" + rect.attributes.getNamedItem("data-gdp").value + " billion";
-            document.getElementById("tooltip").classList.add("visible");
-            document.getElementById("tooltip").classList.remove("invisible");
-            if(rect.x.baseVal.value > 240){
-                document.getElementById("tooltip").style.left = rect.x.baseVal.value - 170 + "px";
-            }
-            else {
-                document.getElementById("tooltip").style.left = 70 + "px";
-            }
-            if(rect.y.baseVal.value > 105){
-                document.getElementById("tooltip").style.top = rect.y.baseVal.value - 10 + "px";
-            }
-            else {
-                document.getElementById("tooltip").style.left = 639.47 + "px";
-                document.getElementById("tooltip").style.top = 96 + "px";
-            }
-        }
-        rect.onmouseleave = () => {
-            document.getElementById("tooltip").classList.remove("visible");
-            document.getElementById("tooltip").classList.add("invisible");
-        }
-    }
-}
-
 function createGraph(data){
-    console.log(data);
     const width = 900;
     const height = 520;
     const margins = {top: 60, right: 50, bottom: 60, left: 50};
     const svg = d3.select("div.panel").append("svg").attr("width", width).attr("height", height);
     const xScale = d3.scaleTime()
-                     .domain([new Date(data.from_date), new Date(data.to_date)])
-                     .range([margins.left,width - margins.right]);
+                        .domain([new Date(data.from_date), new Date(data.to_date)])
+                        .range([margins.left,width - margins.right]);
     const yScale = d3.scaleLinear()
-                     .domain([d3.min(data.data, d => d[1]),d3.max(data.data, d => d[1])])
-                     .range([height - margins.bottom - 10, margins.top]);
+                        .domain([d3.min(data.data, d => d[1]),d3.max(data.data, d => d[1])])
+                        .range([height - margins.bottom - 10, margins.top]);
 
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
@@ -83,3 +51,33 @@ function createGraph(data){
     .text("Source: " + data.source_name);
 
 }
+
+window.addEventListener("load",() => {
+    alert(window.onload);
+    const rects = document.getElementsByTagName("rect");
+    for(let rect of rects){
+        rect.onmouseover = () => {
+            document.getElementById("tooltip").setAttribute("data-date", rect.attributes.getNamedItem("data-date").value);
+            document.getElementById("info").innerText = rect.attributes.getNamedItem("data-date").value + "\n$" + rect.attributes.getNamedItem("data-gdp").value + " billion";
+            document.getElementById("tooltip").classList.add("visible");
+            document.getElementById("tooltip").classList.remove("invisible");
+            if(rect.x.baseVal.value > 240){
+                document.getElementById("tooltip").style.left = rect.x.baseVal.value - 170 + "px";
+            }
+            else {
+                document.getElementById("tooltip").style.left = 70 + "px";
+            }
+            if(rect.y.baseVal.value > 105){
+                document.getElementById("tooltip").style.top = rect.y.baseVal.value - 10 + "px";
+            }
+            else {
+                document.getElementById("tooltip").style.left = 639.47 + "px";
+                document.getElementById("tooltip").style.top = 96 + "px";
+            }
+        }
+        rect.onmouseleave = () => {
+            document.getElementById("tooltip").classList.remove("visible");
+            document.getElementById("tooltip").classList.add("invisible");
+        }
+    }
+})
