7e38e4185dd5259c7777f282e91e39797b560018
user <toropov.nikita2004@gmail.com>
2023-01-29 18:58:39 +0100

    Make responsive

--- bar-chart.js
+++ bar-chart.js
@@ -1,9 +1,38 @@
 import * as d3 from 'https://unpkg.com/d3?module';
 
+let width = document.documentElement.clientWidth/1.2;
+let height = document.documentElement.clientHeight/1.5;
+let margins = {top: 60, right: 60, bottom: 60, left: 60};
+const splitText = "Gross Domestic " + "\nProduct of the USA";
+const singleText = "Gross Domestic Product of the USA";
+determineText();
+
+function determineText(){
+    if(width < 520){
+        document.getElementById("title").innerText = splitText
+        margins ={top: 40, right: 40, bottom: 60, left: 40};
+    }
+    else if(width >= 520) {
+        document.getElementById("title").innerText = singleText;
+    }
+    if(width < 620){
+        margins ={top: 60, right: 40, bottom: 60, left: 40};
+    }
+}
+
 function createGraph(data){
-    const width = 900;
-    const height = 520;
-    const margins = {top: 60, right: 50, bottom: 60, left: 50};
+
+    window.onresize = () => {
+        document.getElementsByClassName("panel")[0].removeChild(document.getElementsByTagName("svg")[0]);
+        width = document.documentElement.clientWidth/1.5;
+        determineText();
+        renderSVG(data,width, height, margins);
+        makeTooltip(margins);
+    }
+    renderSVG(data, width, height, margins);
+}
+
+function renderSVG(data, width, height, margins){
     const svg = d3.select("div.panel").append("svg").attr("width", width).attr("height", height);
     const xScale = d3.scaleTime()
                         .domain([new Date(data.from_date), new Date(data.to_date)])
@@ -15,6 +44,9 @@ function createGraph(data){
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
 
+    if(width < 500){
+        xAxis.ticks(5);
+    }
     svg.append("g")
     .attr("id", "x-axis")
     .attr("transform", "translate(0," + (height - margins.bottom) + ")")
@@ -39,42 +71,44 @@ function createGraph(data){
     .text(data.description.substring(7, 26))
     .style("transform-origin", margins.left+20 + "px " + height/2 + "px")
     .style("transform", "rotate(-90deg)");
-
-    svg.append("text")
+    if(width > 300){
+        svg.append("text")
     .attr("x", width - 300)
     .attr("y", height - 10)
-    .attr("margin", "10px")
+    .attr("class", "source")
     .text("Source: " + data.source_name);
+    }
+}
+
+function makeTooltip(margins){
+    const rects = document.getElementsByTagName("rect");
+    for(let rect of rects){
+        rect.onmouseover = () => {
+            document.getElementById("tooltip").setAttribute("data-date", rect.attributes.getNamedItem("data-date").value);
+            document.getElementById("info").innerText = rect.attributes.getNamedItem("data-date").value + "\n$" + rect.attributes.getNamedItem("data-gdp").value + " billion";
+            document.getElementById("tooltip").classList.add("visible");
+            document.getElementById("tooltip").classList.remove("invisible");
+            if(rect.x.baseVal.value - 180 > margins.left){
+                document.getElementById("tooltip").style.left = rect.x.baseVal.value - 170 + "px";
+            }
+            else{
+                document.getElementById("tooltip").style.left = margins.left + 10 + "px";
+            }
+            if(rect.y.baseVal.value - 30> margins.top){
+                document.getElementById("tooltip").style.top = rect.y.baseVal.value - 10 + "px";
+            }
+            else{
+                document.getElementById("tooltip").style.top = margins.top + 20 + "px";
+            }
+        }
+        rect.onmouseleave = () => {
+            document.getElementById("tooltip").classList.remove("visible");
+            document.getElementById("tooltip").classList.add("invisible");
+        }
+    }
 }
 
 fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json")
     .then(response => response.json())
     .then(d => createGraph(d))
-    .then(() => {
-        const rects = document.getElementsByTagName("rect");
-        for(let rect of rects){
-            rect.onmouseover = () => {
-                document.getElementById("tooltip").setAttribute("data-date", rect.attributes.getNamedItem("data-date").value);
-                document.getElementById("info").innerText = rect.attributes.getNamedItem("data-date").value + "\n$" + rect.attributes.getNamedItem("data-gdp").value + " billion";
-                document.getElementById("tooltip").classList.add("visible");
-                document.getElementById("tooltip").classList.remove("invisible");
-                if(rect.x.baseVal.value > 240){
-                    document.getElementById("tooltip").style.left = rect.x.baseVal.value - 170 + "px";
-                }
-                else {
-                    document.getElementById("tooltip").style.left = 70 + "px";
-                }
-                if(rect.y.baseVal.value > 105){
-                    document.getElementById("tooltip").style.top = rect.y.baseVal.value - 10 + "px";
-                }
-                else {
-                    document.getElementById("tooltip").style.left = 639.47 + "px";
-                    document.getElementById("tooltip").style.top = 96 + "px";
-                }
-            }
-            rect.onmouseleave = () => {
-                document.getElementById("tooltip").classList.remove("visible");
-                document.getElementById("tooltip").classList.add("invisible");
-            }
-        }
-    });
+    .then(() => makeTooltip(margins));
